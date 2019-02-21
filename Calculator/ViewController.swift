23b4ee2d41cc8ce13b43257f1675c7c9ebcc0c5b
Egor Tkachenko <egortka@egors-imac.local>
2019-02-21 15:54:42 +0700

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Egor Tkachenko on 21/02/2019.
+//  Copyright © 2019 ET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
